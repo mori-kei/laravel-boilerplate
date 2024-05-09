bd3d08a0cbd3bb30fd0e7202f1fbff5b4e802ad8
mori-kei <morimeru13@gmail.com>
2024-05-09 08:23:50 +0000

    teamの作成、更新時のバリデーションエラーの対応

--- resources/views/manager/teams/edit.blade.php
+++ resources/views/manager/teams/edit.blade.php
@@ -8,7 +8,10 @@
             @method('patch')
             <div class="mb-3">
                 <label class="form-label" for="teamName">名前</label>
-                <input type="text" name="name" value="{{ old('name', $team->name) }}" id="teamName" class="form-control">
+                <input type="text" name="name" value="{{ old('name', $team->name) }}" id="teamName" class="form-control @error('name') is-invalid @enderror">
+                @error('name')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
             <input type="submit" value="更新" class="btn btn-primary">
         </form>
