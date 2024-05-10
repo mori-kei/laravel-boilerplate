16fb8aa8c5860ce532c7789473d3a8eadd383227
mori-kei <morimeru13@gmail.com>
2024-05-10 01:38:43 +0000

    tasks,teamsの入力欄にold関数

--- resources/views/manager/teams/create.blade.php
+++ resources/views/manager/teams/create.blade.php
@@ -7,7 +7,7 @@
             @csrf
             <div class="mb-3">
                 <label class="form-label" for="teamName">名前</label>
-                <input type="text" name="name" value="" id="teamName" class="form-control @error('name') is-invalid @enderror"> 
+                <input type="text" name="name" value="{{ old('name') }}" id="teamName" class="form-control @error('name') is-invalid @enderror"> 
                 @error('name')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
--- resources/views/manager/teams/tasks/create.blade.php
+++ resources/views/manager/teams/tasks/create.blade.php
@@ -7,14 +7,14 @@
             @csrf
             <div class="mb-3">
                 <label class="form-label" for="taskTitle">タイトル</label>
-                <input type="text" name="title" value="" id="taskTitle" class="form-control @error('title') is-invalid @enderror"> 
+                <input type="text" name="title" value="{{ old('title') }}" id="taskTitle" class="form-control @error('title') is-invalid @enderror"> 
                 @error('title')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="mb-3">
                 <label class="form-label" for="taskBody">内容</label>
-                <textarea type="text" name="body" value="" id="taskBody" class="form-control @error('body') is-invalid @enderror"></textarea> 
+                <textarea type="text" name="body" value="" id="taskBody" class="form-control @error('body') is-invalid @enderror">{{ old('body') }}</textarea> 
                 @error('body')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
