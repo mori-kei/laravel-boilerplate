f568f172eedf40cf1690c58d419f3490dee0bc6e
mori-kei <morimeru13@gmail.com>
2024-05-13 08:16:51 +0000

    Task一覧の表示項目に担当者の情報を追加

--- resources/views/manager/teams/show.blade.php
+++ resources/views/manager/teams/show.blade.php
@@ -14,6 +14,7 @@
                 <tr>
                     <th scope="col">タスクid</th>
                     <th scope="col">タイトル</th>
+                    <th scope="col">担当者</th>
                     <th scope="col">作成日時</th>
                     <th scope="col">操作</th>
                 </tr>
@@ -23,6 +24,7 @@
                     <tr>
                         <th scope="row">{{ $task->id }}</th>
                         <td>{{ $task->title }}</td>
+                        <td>{{ $task->assignedTo ? $task->assignedTo->name : 'なし' }}</td>
                         <td scope="row">{{ $task->created_at }}</td>
                         <td><a href="{{ route('manager.teams.tasks.edit', ['team' => $team->id,'task' => $task->id]) }}" class="btn btn-primary btn-sm">編集</a></td>
                     </tr>
