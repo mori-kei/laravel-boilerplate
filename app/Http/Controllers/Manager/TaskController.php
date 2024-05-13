284949af5320e62e205d18a79e4275fd147a78bd
mori-kei <morimeru13@gmail.com>
2024-05-13 09:13:35 +0000

    editと書き方を統一

--- app/Http/Controllers/Manager/TaskController.php
+++ app/Http/Controllers/Manager/TaskController.php
@@ -44,11 +44,11 @@ class TaskController extends Controller
         $validated = $request->validate([
             'title' => 'required', 
             'body' => 'required', 
+            'assignee_id' => ''
         ]);
         $team_id = $team->id;
         $task = new Task($validated);
         $task->team_id = $team_id;
-        $task->assignee_id= $request->assignee_id;
         $task->save();
         return to_route('manager.teams.show', $team_id)->with('success', 'タスクを作成しました');
     }
