1b951c9f72b5e670e356a4a53acc76c0cdc6e4a2
mori-kei <morimeru13@gmail.com>
2024-05-14 05:38:37 +0000

    createページのアクション属性の変更

--- resources/views/manager/teams/create.blade.php
+++ resources/views/manager/teams/create.blade.php
@@ -3,7 +3,7 @@
     <h2>チーム新規作成</h2>
     <x-form-error />
     <x-mini-panel>
-        <form action="{{ route('manager.teams.store') }}" method="post">
+        <form action="{{ route('teams.store') }}" method="post">
             @csrf
             <div class="mb-3">
                 <label class="form-label" for="teamName">名前</label>
