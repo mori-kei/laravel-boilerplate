3fb7475be6283bb4b874d81dffaf193e3ab670b7
mori-kei <morimeru13@gmail.com>
2024-05-14 05:09:17 +0000

    不要なセミコロンの削除

--- database/migrations/2024_05_13_061800_add_assignee_id_to_tasks.php
+++ database/migrations/2024_05_13_061800_add_assignee_id_to_tasks.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->unsignedBigInteger('assignee_id')->nullable();;
+            $table->unsignedBigInteger('assignee_id')->nullable();
             $table->foreign('assignee_id')->references('id')->on('users');
         });
     }
