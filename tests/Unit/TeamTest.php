cef1026c07e45c50b5e9f6ce0d029caec489d776
mori-kei <morimeru13@gmail.com>
2024-05-16 02:05:42 +0000

    TeamとMemberがそれぞれ1件増えているかを確認する記述を追加

--- tests/Unit/TeamTest.php
+++ tests/Unit/TeamTest.php
@@ -16,11 +16,15 @@ class TeamTest extends TestCase
         $data = [
             'name' => 'dummy name',
         ];
+        $team_count = Team::count();
+        $member_count = Member::count();
         $team = Team::createWithOwner($user, $data);
+        $this->assertEquals($team_count + 1, Team::count()); 
         $this->assertNotNull($team);
         $this->assertEquals('dummy name', $team->name);
         $this->assertEquals($user->id, $team->owner_id); 
         $storedMember = $team->members()->latest()->first();
+        $this->assertEquals($member_count + 1, Member::count()); 
         $this->assertEquals($team->id,$storedMember->team_id);
         $this->assertEquals($user->id,$storedMember->user_id);
         $this->assertEquals(1,$storedMember->role);
