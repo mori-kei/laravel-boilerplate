d083dee3a305c15b01e5676f9baa5bb2ce851525
mori-kei <morimeru13@gmail.com>
2024-05-16 01:25:21 +0000

    使用していないuseの記述を削除

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Team;
-use App\Models\Member;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class TeamController extends Controller
 {
