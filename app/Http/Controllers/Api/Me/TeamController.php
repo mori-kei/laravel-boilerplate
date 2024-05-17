481cbb9c83343b20368fa31fac4ea812f54573a6
mori-kei <morimeru13@gmail.com>
2024-05-17 06:51:45 +0000

    200番を返すように実装

---
+++ app/Http/Controllers/Api/Me/TeamController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api\Me;
+
+use App\Http\Controllers\Controller;
+use App\Models\Task;
+use App\Models\Team;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class TeamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(['message' => 'Success'], 200);
+    }
+}
