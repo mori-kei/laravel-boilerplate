e3dce63aa2000e7f9456f8fa5d86c50f60b44f47
mori-kei <morimeru13@gmail.com>
2024-05-28 07:37:13 +0000

    Issue11. [Worker][Vue]ログインユーザーに関連するチーム/タスクを表示する

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,93 @@
 <template>
   <div class="home">
+    <h2>アサインされているタスク</h2>
+    <table>
+      <thead >
+        <th>チーム</th>
+        <th>タスクID</th>
+        <th>タイトル</th>
+        <th>担当者</th>
+        <th>作成日時</th>
+        <th>操作</th>
+      </thead>
+      <tbody >
+        <tr v-for="(mytask, id) in mytasks" :key="id">
+          <td>  {{mytask.team_id}}</td>
+          <td>
+            {{mytask.id}}
+          </td>
+          <td>
+            {{mytask.title}}
+          </td>
+          <td>
+            {{mytask.assignee_id}}
+          </td>
+          <td>
+            {{mytask.created_at}}
+          </td>
+        </tr>
+    </tbody>
+    </table>
+    <h2>所属しているチーム</h2>
+    <table>
+      <thead >
+        <th>チームID</th>
+        <th>チーム名</th>
+      </thead>
+      <tbody >
+        <tr v-for="(myteam, id) in myteams" :key="id">
+          <td>  {{myteam.id}}</td>
+          <td>
+            {{myteam.name}}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
 export default {
   name: 'HomeView',
+  setup(){
+    const mytasks = ref({})
+    const myteams = ref({})
+
+    const fetchMyTasks = async() => {
+      const url = "http://localhost:8080/api/me/tasks"
+      const res = await axios.get(url)
+      mytasks.value = res.data
+    }
+
+    const fetchMyTeams = async() => {
+      const url = "http://localhost:8080/api/me/teams"
+      const res = await axios.get(url)
+      myteams.value =res.data
+    }
+
+    onMounted(() => {
+      fetchMyTasks();
+      fetchMyTeams();
+    })
+    return {
+      mytasks,
+      myteams
+    }
+  }
 }
 </script>
+
+<style scoped>
+table{
+  width: 100%;
+}
+th{
+  background-color: #D3D3D3;
+}
+td{
+  text-align: center;
+}
+</style>
