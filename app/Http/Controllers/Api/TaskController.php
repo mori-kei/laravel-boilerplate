b873af95500c83030c3dcfa82612087a31dd0b11
mori-kei <morimeru13@gmail.com>
2024-05-29 08:30:48 +0000

    showメソッドでteam_nameも返すように変更

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -10,6 +10,18 @@ class TaskController extends Controller
 {
     public function show(Task $task)
     {
-        return response()->json($task);
+        $teamName = $task->team->name;
+        return response()->json([
+            'task' => [
+                'id' => $task->id,
+                'title' => $task->title,
+                'body' => $task->body,
+                'status' => $task->status,
+                'assignee_id' => $task->assignee_id,
+                'created_at' => $task->created_at->toDateTimeString(),
+                'updated_at' => $task->updated_at->toDateTimeString(),
+                'team_name' => $teamName
+            ],
+        ]);
     }
 }
