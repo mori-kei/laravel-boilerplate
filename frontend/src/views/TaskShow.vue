82bb659a7aed33b96f596046573b1e5fb9994817
mori-kei <morimeru13@gmail.com>
2024-05-30 03:54:58 +0000

    taskからteamに関する情報を取得するように修正

--- frontend/src/views/TaskShow.vue
+++ frontend/src/views/TaskShow.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h2>{{team.name}}/{{task.title }}</h2>
+        <h2 v-if="task.team">{{task.team.name}}/{{task.title}}</h2>
         <h3>内容</h3>
         <p>{{task.body }}</p>
     </div>
@@ -14,21 +14,18 @@ export default {
     name:'TaskShow',
     setup() {
         const task = ref([]);
-        const team = ref([]);
         const route = useRoute();
         const id = route.params.id;
         const fetchTask = async () => {
             const url = `http://localhost:8080/api/tasks/${id}`
             const res = await axios.get(url)
             task.value = res.data.task
-            team.value = res.data.task.team
         }
         onMounted(async () => {
             await fetchTask()
         })
         return {
             task,
-            team
         }
     },
 }
