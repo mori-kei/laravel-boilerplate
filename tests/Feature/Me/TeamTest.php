9278baf44ef0810d6f4c44aff5f18998b7c10077
mori-kei <morimeru13@gmail.com>
2024-05-31 04:39:43 +0000

    user_idのテスト項目を追加

--- tests/Feature/Me/TeamTest.php
+++ tests/Feature/Me/TeamTest.php
@@ -36,14 +36,20 @@ class MeTeamTest extends TestCase
         ]);
         $response->assertJson([0 => [
             'members' => [
-                ['role' => 1],
+                [
+                    'role' => 1,
+                    'user_id' => $user1->id
+                ],
             ],
             'id' => $team1->id,
             'name' => 'dummy name',
         ]]);
         $response->assertJson([1 => [
             'members' => [
-                ['role' => 0],
+                [
+                'role' => 0,
+                'user_id' => $user1->id
+                ],
             ],
             'id' => $team2->id,
             'name' => 'dummy name2',
