6e82aec88cea4cb41b4fa36a3dc06bc15e9e00cf
mori-kei <morimeru13@gmail.com>
2024-06-04 06:06:02 +0000

    Promise.allを使用するように変更

--- frontend/src/views/TaskShow.vue
+++ frontend/src/views/TaskShow.vue
@@ -1,22 +1,27 @@
 <template>
-    <div class="parent">
-        <div v-if="task.status ===1" class="flash">
-            <p>このタスクは完了しました</p>
-        </div>
-        <h2 v-if="task.team">{{task.team.name}}/{{task.title}}</h2>
-        <div class="content">
-            <h3>内容</h3>
-            <p>{{task.body }}</p>
-            <h3>コメント</h3>
-            <div v-for="comment in comments" :key="comment.id" class="comment">
-                <p>{{comment.message}}</p>
-                <p>{{comment.created_at}} by {{comment.authorname }}</p>
+    <div >
+        <div class="parent" v-if="isLogin">
+            <div v-if="task.status ===1" class="finish">
+                <p>このタスクは完了しました</p>
             </div>
-            <p>本文</p>
-            <textarea v-model="message" class="comment_txt"></textarea>
-            <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
-            <p v-if="task && task.status === 0">完了報告とする<input type="checkbox" v-model="kind"></p>
-            <button v-on:click="createComment" class="btn btn-primary">送信</button>
+            <h2 v-if="task.team">{{task.team.name}}/{{task.title}}</h2>
+            <div class="content">
+                <h3>内容</h3>
+                <p>{{task.body }}</p>
+                <h3>コメント</h3>
+                <div v-for="comment in comments" :key="comment.id" class="comment">
+                    <p>{{comment.message}}</p>
+                    <p>{{comment.created_at}} by {{comment.authorname }}</p>
+                </div>
+                <p>本文</p>
+                <textarea v-model="message" class="comment_txt"></textarea>
+                <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
+                <p v-if="task && task.status === 0">完了報告とする<input type="checkbox" v-model="kind"></p>
+                <button v-on:click="createComment" class="btn btn-primary">送信</button>
+            </div>
+        </div>
+        <div v-else>
+            aaa
         </div>
     </div>
 </template>
@@ -25,7 +30,7 @@
 import axios from 'axios';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
-
+const isLogin = ref(false)
 const task = ref({});
 const comments = ref({})
 const message = ref("")
@@ -36,13 +41,12 @@ const id = route.params.id;
 const fetchTask = async () => {
     const url = `http://localhost:8080/api/tasks/${id}`
     const res = await axios.get(url)
-    task.value = res.data.task
-    console.log(task)
+    return res.data.task;
 }
 const fetchComments = async () => {
     const url = `http://localhost:8080/api/tasks/${id}/comments`
     const res = await axios.get(url)
-    comments.value = res.data.comments
+    return res.data.comments;
 }
 const createComment = async () => {
     try {
@@ -52,6 +56,7 @@ const createComment = async () => {
             kind: kind.value ? 1 : 0,
         });
         comments.value.push(res.data[0]);
+        console.log(res.data[0]);
         message.value = "";
     } catch (error) {
         if (error.response && error.response.status === 422) {
@@ -63,8 +68,14 @@ const createComment = async () => {
 };
 
 onMounted(async () => {
-    await fetchTask()
-    await fetchComments()
+    try {
+        const [taskData, commentsData] = await Promise.all([fetchTask(), fetchComments()]);
+        task.value = taskData;
+        comments.value = commentsData;
+        isLogin.value = true
+    } catch (error) {
+        isLogin.value = false
+    }
 })
 
 
@@ -74,7 +85,7 @@ onMounted(async () => {
     background-color: #F8F9FA;
     padding: 5px;
 }
-.flash {
+.finish {
     background-color: #CEF4FC;
     color: #115160;
     padding: 5px;
